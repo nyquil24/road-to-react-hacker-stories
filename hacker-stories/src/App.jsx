dae2e929ec87016a9ea2112acfa23aa341231c4e
Opie Taylor <taborn.nequil@gmail.com>
2024-11-22 14:40:54 -0600

    week3

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -6,24 +6,7 @@ function getTitle(title){
   return title; 
 }
 
-const list = [
-{
-  title: 'React', 
-  url: 'https://reatjs.org/', 
-  author: 'Jordan Walke', 
-  num_comments: 3, 
-  points: 4, 
-  objectID: 0, 
-},
-{
-  title: 'Redux', 
-  url: 'https://redux.js.org/', 
-  author: 'Dan Abramov, Andrew Clark', 
-  num_comments: 2, 
-  points: 5, 
-  objectID: 1, 
-}
-];
+
 
 class Person {
   constructor(firstName, lastName){
@@ -45,47 +28,83 @@ console.log(dennis.getName());
 
 
 const App = () =>{
+  const stories = [
+    {
+      title: 'React', 
+      url: 'https://reatjs.org/', 
+      author: 'Jordan Walke', 
+      num_comments: 3, 
+      points: 4, 
+      objectID: 0, 
+    },
+    {
+      title: 'Redux', 
+      url: 'https://redux.js.org/', 
+      author: 'Dan Abramov, Andrew Clark', 
+      num_comments: 2, 
+      points: 5, 
+      objectID: 1, 
+    }
+    ];
+
+    const handleSearch = (event) => { 
+      console.log(event.target.value);
+    }
+
   return(
       <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
       
       <hr/>
-
-     <List/>
+        {/*using props to pass the list of items to the List component. */} 
+     <List list={stories}/>
       
       </div>
     )
 }
 
 //Adding new List() Component 
-const List = () =>{
-  return (
-    <ul>
-    {list.map((item) => {
-      return ( 
-      <li key={item.objectID}>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      </li>
-    );
-    })}
-    </ul>
-  );
-}
+//props in React allow you to pass variables as information from one component to another.
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+); 
+
+const Item = (props) => (
+  <li>
+    <span> 
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 //Search Component 
-const Search = () =>{
+const Search = (props) =>{
+  //useState is telling react that we want to have a stateful value which changes over time
+  const [searchTerm, setSearchTerm] = React.useState(''); 
+
+  const handleChange = (event) => {
+   const [searchTerm, setSearchTerm] = React.useState(''); 
+
+   const handleChange = (event) => {
+    setSearchTerm(event.target.value); 
+
+    props.onSearch(event); 
+   }
+  }
   return(
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" /> 
-    </div>
+      <input id="search" type="text" onChange={handleChange}/> 
+    </div> 
   )
 }
 
