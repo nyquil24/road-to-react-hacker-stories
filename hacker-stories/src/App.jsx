43ebd932928937654c9c3709481a124088df70f1
Opie Taylor <taborn.nequil@gmail.com>
2025-01-08 13:15:10 -0600

    React Asynchronous Data

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,111 +1,120 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import * as React from 'react'; 
+/* eslint-disable react/prop-types */
+import * as React from 'react';
 
-//const title = 'React'; 
-
-function getTitle(title){
-  return title; 
-}
-
-
-
-class Person {
-  constructor(firstName, lastName){
-    this.firstName = firstName; 
-    this.lastName = lastName; 
-  }
-
-  getName(){
-    return this.firstName + ' ' + this.lastName; 
-  }
-
-}
-
-const robin = new Person('Robin', 'Wieruch');
-console.log(robin.getName()); 
-
-const dennis = new Person('Dennis', 'Wieruch'); 
-console.log(dennis.getName());
-
-
-const App = () =>{
+const App = () => {
   const stories = [
     {
-      title: 'React', 
-      url: 'https://reatjs.org/', 
-      author: 'Jordan Walke', 
-      num_comments: 3, 
-      points: 4, 
-      objectID: 0, 
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
     },
     {
-      title: 'Redux', 
-      url: 'https://redux.js.org/', 
-      author: 'Dan Abramov, Andrew Clark', 
-      num_comments: 2, 
-      points: 5, 
-      objectID: 1, 
-    }
-    ];
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-    const [searchTerm, setSearchTerm] = React.useState('React'); 
 
-    const handleSearch = (event) => { 
-      setSearchTerm(event.target.value);
-    }
+  const [searchTerm, setSearchTerm] = useStorageState(
+    'search', 
+    'React'
+  )
+  
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
-    const searchedStories = stories.filter((story)=> 
-    story.title.toLowerCase().includes(searchTerm.toLowerCase()
-  ))
 
-  return(
-      <div>
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+      localStorage.getItem('key') || initialState
+    )
+
+    React.useEffect(() => {
+      localStorage.setItem(key, value);
+    }, [value, key]); 
+    return [value, setValue]; 
+  }
+
+  return (
+    <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch}/>
-      
-      <hr/>
-        {/*using props to pass the list of items to the List component. */} 
-     <List list={searchedStories}/>
-      
-      </div>
-    )
-}
+      <InputWithLabel
+      id="search"
+      label="Search"
+      value={searchTerm}
+      onInputChange={handleSearch}
+      />
 
-const Search = ({search, onSearch}) => {
- 
-  return(
-  <div> 
-    <label htmlFor="search">Search: </label>
-    <input id="search" 
-    type="text" 
-    value={search}
-    onChange={onSearch} />
-  
-  </div>
+      <Search search={searchTerm} onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const InputWithLabel = ({id, label, value, type = 'text', onInputChange}) => (
+  <>
+  <label htmlFor={id}>{label}</label>
+  &nbsp; 
+  <input
+    id={id}
+    type="text"
+    value={value}
+    onChange={onInputChange}
+    />
+  </>
 )
-}
-//Adding new List() Component 
-//props in React allow you to pass variables as information from one component to another.
-const List = ({list}) => (
+
+const Search = ({ search, onSearch }) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </div>
+);
+
+const List = ({ list }) => (
   <ul>
     {list.map((item) => (
-     <Item key={item.objectID} {...item} /> 
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
-); 
+);
 
-const Item = ({title, url,author,num_comments, points}) => (
+const Item = ({ item }) => (
   <li>
-    <span> 
-      <a href={url}>{title}</a>
+    <span>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
-
-export default App; 
+export default App;
